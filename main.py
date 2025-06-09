82b62b5cab06ee1856c31219af65471dc8e3fa98
vachan-maker <vachan@tutanota.com>
2025-06-09 21:55:38 +0530

    basic structure for main.py

--- main.py
+++ main.py
@@ -2,4 +2,12 @@ from fastapi import FastAPI
 from . import models,database
 from sqlalchemy.orm import Session
 
+models.Base.metadata.create_all(database.engine)
 app = FastAPI()
+
+def get_db():
+    db = database.SessionLocal() # for creating a new local connection
+    try:
+        yield db
+    finally:
+        db.close()
